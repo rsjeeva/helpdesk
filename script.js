a55f43ab93931f3c046b8d2561ebbbcff1f3bc9e
rsjeeva <75556646+rsjeeva@users.noreply.github.com>
2021-05-30 13:40:27 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+var ticket_list = document.getElementById("ticket_list");
+
+
+// newaccount1622276654516
+
+
+// var req = new Request("https://newaccount1622276654516.freshdesk.com/api/v2/tickets",myInit);
+
+
+
+fetch("https://newaccount1622276654516.freshdesk.com/api/v2/tickets").then(req=>{
+    console.log(req);
+});
+
+
+
+// 4RFu9e29IgMj3UvTdPD
